32d4e64abb71a3a4d09d8de3a70e75a25456661d
Daniel Somade <flexsidee@gmail.com>
2025-04-09 11:18:22 +0100

    bot responds to message in private and supergroup

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 # Load environment variables from .env file
 load_dotenv()
 
-# Load environment variables from .env file
+# Configurations
 TOKEN = os.getenv("BOT_TOKEN")
 BOT_USERNAME = os.getenv("BOT_USERNAME")
 
@@ -20,6 +20,10 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Somade Daniel Testing the help command")
 
 
+async def add_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"This is a response from add command")
+
+
 #  Responses
 def handle_response(text: str) -> str:
     processed: str = text.lower()
@@ -28,6 +32,8 @@ def handle_response(text: str) -> str:
         return "Hey there"
     if 'purpose' in processed:
         return "This bot is built for testing"
+    if 'name' in processed:
+        return "My name is Somade's Bot"
     return "I do not understand what you typed"
 
 
@@ -35,7 +41,20 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
 
-    print(f"User ({update.message.chat.id} in {message_type}: '{text}'")
+    print(f"User ({update.message.chat.id}) in {message_type}: '{text}'")
+
+    if message_type == 'supergroup':
+        if BOT_USERNAME in text:
+            print('bot called')
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print(f'Bot: {response} \n--------')
+    await update.message.reply_text(response)
 
 
 # getting error
@@ -43,16 +62,17 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f"Update {update} caused error {context}")
 
 
-if __name__ == '__main__':
+def main():
     print('Starting bot...')
     app = Application.builder().token(TOKEN).build()
 
     # Commands
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('add', add_command))
 
     # Messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # Errors
     app.add_error_handler(error)
@@ -60,3 +80,7 @@ if __name__ == '__main__':
     # Polls the bot
     print('Polling...')
     app.run_polling(poll_interval=3)
+
+
+if __name__ == '__main__':
+    main()
